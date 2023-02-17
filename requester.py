8a7ed4150e29be43c529051699c3d5996e1b1981
Charmaine Seah <charmaineseahsy@gmail.com>
2023-02-17 16:23:31 -0600

    headers being sent from sender to requester using struct.pack()

--- requester.py
+++ requester.py
@@ -1,6 +1,18 @@
 import socket
+import struct 
+
+def printReceiptInformation(header, data):
+    print("time received: ")
+    print("sender's IP address: ", sender_address[0], " sender's port: ", sender_address[1])
+    print("packet type: ", header[0].decode('ascii'))
+    print("sequence number: ", header[1])
+    print("payload length: ", header[2])
+    print("first 4 bytes of the payload: ", data[:4].decode("utf-8"))
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 1) parse command line args and get requester port number and file to request name
+
+# 2) read tracker.txt and retrieve sender name and port number and other details
 
 udp_host = socket.gethostname()
 udp_port = 12345
@@ -9,5 +21,12 @@ sock.bind((udp_host, udp_port))
 
 while True:
     print("Waiting for client...")
-    data, addr = sock.recvfrom(1024)
-    print("Received messages: ", data, " from: ", addr)
+
+    packet_with_header, sender_address = sock.recvfrom(1024)
+    header = struct.unpack("!cII", packet_with_header[:9])
+    data = packet_with_header[9:]
+
+    printReceiptInformation(header, data)
+
+    
+    
--- sender.py
+++ sender.py
@@ -1,13 +1,21 @@
 import socket
+import struct
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 udp_host = socket.gethostname()
 udp_port = 12345
 
-data = "Hello World! My name is Charmaine."
-packet = data.encode()
+data = "Hello World! My name is Charmaine.".encode()
 print("UDP target IP: ", udp_host)
 print("UDP target port: ", udp_port)
 
-sock.sendto(packet, (udp_host, udp_port))
+# assemble udp header
+packet_type = 'R'.encode('ascii')
+sequence_number = 1112
+data_length = len(data) 
+udp_header = struct.pack("!cII", packet_type, sequence_number, data_length)
+
+packet_with_header = udp_header + data
+
+sock.sendto(packet_with_header, (udp_host, udp_port))
